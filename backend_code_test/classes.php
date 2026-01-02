045b6755570cff0924c9d7bf8611b98c3ba1f4ed
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-02 21:25:09 +0100

    set up all photo class attributes

--- backend_code_test/classes.php
+++ backend_code_test/classes.php
@@ -35,5 +35,22 @@ class user
 }
 
 class photo {
-    
+    private DateTime $updatedAt;
+    private DateTime $publishedAt;
+    private DateTime $createdAt;
+
+    public function __construct(
+    private int $id,
+    private string $title,
+    private string $description,
+    private string $imageLink,
+    private int $fileSize,
+    private string $dimensions,
+    private string $state,
+    private int $viewCount ,
+    private int $userId
+    )
+    {
+
+    }
 }
