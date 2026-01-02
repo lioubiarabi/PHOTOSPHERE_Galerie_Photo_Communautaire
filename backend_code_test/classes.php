5bc07838e9bb1590a02572ba64f80af2a75a710f
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-02 21:08:11 +0100

    set tup all the user class attributes

--- backend_code_test/classes.php
+++ backend_code_test/classes.php
@@ -1,6 +1,20 @@
 <?php
 
 class user {
-    
+    public function __construct(
+        private int $id,
+        private string $username,
+        private string $email,
+        private string $password,
+        private string $role,
+        private string $profilePicture,
+        private DateTime $cratedAt,
+        private ?string $level,
+        private ?int $uploadCount,
+        private ?int $resetDate,
+        private ?DateTime $lastrLogin
+    ) {
+        
+    }
 }
 ?>
