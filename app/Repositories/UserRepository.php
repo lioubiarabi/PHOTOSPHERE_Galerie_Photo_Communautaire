3bb0a21c73633387a96c64db2dc9db4be954f932
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-05 14:30:45 +0100

    userRepositpry classes

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,48 @@
+<?php
+require 'config.php';
+require '../Entities/User.php';
+require '../Entities/BasicUser.php';
+require '../Entities/ProUser.php';
+require '../Entities/Moderator.php';
+require '../Entities/Admin.php';
+
+
+class UserRepository implements RepositoryInterface {
+    private ?User $current_user = null;
+    public function __construct(
+        private PDO $pdo
+    ){}
+    
+    public function login(string $username, string $password): bool
+    {
+        return true;
+    }
+
+    public function isLoggedIn(): bool
+    {
+        return true;
+    }
+
+    public function logout(): bool
+    {
+        return true;
+    }
+    
+    public function addUser(User $user): bool
+    {
+        return true;
+    }
+
+    public function updateUser(User $user): bool
+    {
+        return true;
+    }
+
+    public function removeUser(int $index): bool
+    {
+        return true;
+    }
+
+};
+
+$db = new UserRepository($pdo);
