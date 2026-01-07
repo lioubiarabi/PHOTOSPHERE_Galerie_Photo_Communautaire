76ffcbb71c290696e2a161cf56e44d946564406f
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-07 09:39:50 +0100

    add the interfaces

---
+++ app/Interfaces/Commentable.php
@@ -0,0 +1,12 @@
+<?php
+
+interface Commentable
+{
+    public function addComment(string $content, int $userId): int;
+
+    public function removeComment(int $commentId): bool;
+
+    public function getComments(): array;
+
+    public function getCommentCount(): int;
+};
---
+++ app/Interfaces/Likeable.php
@@ -0,0 +1,14 @@
+<?php
+
+interface Likeable
+{
+    public function addLike(int $userId): bool;
+
+    public function removeLike(int $userId): bool;
+
+    public function isLikedBy(int $userId): bool;
+
+    public function getLikeCount(): int;
+
+    public function getLikedBy(): array;
+};
